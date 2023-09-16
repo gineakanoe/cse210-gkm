d0d64d25e3d85f9517daa947952069a3c2ba883f
Ginea Merrill <gineakanoe@gmail.com>
2023-09-16 11:28:13 -0400

    did prep 2 in prepare

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        string inputFromUser = Console.ReadLine();
+
+        // Convert to integer
+        int x = int.Parse(inputFromUser);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations!  You have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass; good luck next go round!");
+        }
+
     }
 }
