1efd29caaffd79906acf52d86672a0058475ecfb
Ginea Merrill <gineakanoe@gmail.com>
2023-09-22 19:55:05 -0400

    stubbed out journal program

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -2,5 +2,26 @@ using System;
 
 public class Journal
 {
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
     
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal theJurnal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+
         Console.WriteLine("Hello Develop02 World!");
     }
 }
