260434b85dde5334312e753b46c4418cbf364c14
Ginea Merrill <gineakanoe@gmail.com>
2024-04-26 22:16:14 -0400

    got prep 3 to work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string userInput = Console.ReadLine();
+        // int magicNumber = int.Parse(userInput);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            guess = int.Parse(userInput2);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+            }
+        }
+
     }
 }
